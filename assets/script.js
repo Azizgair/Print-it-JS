9ba0b36dbfcd6d6f42d12b0f00c2ba0a9ce37f14
Abdelaziz HAMMAD <39907878+Azizgair@users.noreply.github.com>
2023-12-03 10:15:42 +0100

    carrousel tourne en boucle indéfiniment

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,16 @@ const slides = [
 ]
 
 let currentSlide = 0;
+let autoSlideInterval;
+
+function startAutoSlide() {
+	autoSlideInterval = setInterval(() => {
+		nextSlide();
+	}, 2000);
+}
+function stopAutoSlide() {
+	clearInterval(autoSlideInterval);
+}
 
 function showSlide(index) {
 	const slidesContainer = document.querySelector('.slides-container');
@@ -42,20 +52,45 @@ function updateDots(index) {
 		if (i === index) {
 			dot.classList.add('dot_selected');
 		}
-		dot.onclick = () => showSlide(i);
+		dot.onclick = () => {
+			stopAutoSlide();
+			showSlide(i);
+		};
 		dotsContainer.appendChild(dot);
+	}
+}
+
+function handleLoop(index) {
+	if (index < 0) {
+		currentSlide = slides.length - 1;
+	} else if (index >= slides.length) {
+		currentSlide = 0;
 	}
 }
 
 function prevSlide() {
 	currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+	handleLoop(currentSlide);
 	showSlide(currentSlide);
 }
 
 function nextSlide() {
 	currentSlide = (currentSlide + 1) % slides.length;
+	handleLoop(currentSlide);
 	showSlide(currentSlide);
 }
+document.addEventListener('DOMContentLoaded', () => {
+	startAutoSlide();
+});
 
+document.querySelector('.arrow_left').addEventListener('click', () => {
+	stopAutoSlide();
+	prevSlide();
+});
+
+document.querySelector('.arrow_right').addEventListener('click', () => {
+	stopAutoSlide();
+	nextSlide();
+});
 
 showSlide(currentSlide);
